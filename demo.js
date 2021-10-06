1935b66ce37f7790369a0ec617bf1394e4a8bfa7
Hien0000 <91872436+Hien0000@users.noreply.github.com>
2021-10-06 16:05:04 +0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,14 @@
+//const strings = ['hello']
+//const mặc định không đổi đc
+//stirngs là tên biến đặt tên gì cũng đc
+
+//strings.push('word')
+//push là thêm vào dữ liệu 
+//muốn gán vào đâu thì gán biến đó ngay đầu 
+
+//console.log(strings)
+//in ra câu bạn muốn
+
+const fruit = ['orange','pineapple','apple']
+fruit.push('cheese')
+console.log(fruit)
